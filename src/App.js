c816c89e2a89b0625cc5d6a06dfcdf06c0676c46
R3BEL <vasco8990@gmail.com>
2018-06-15 14:51:57 +0700

    Working calculator commit

--- src/App.js
+++ src/App.js
@@ -1,81 +1,173 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
+class Calculator extends React.Component {
+  state = {
+    value: null,
+    displayValue: "0",
+    waitingForOperand : false,
+    operator: null
+  };
+
+clearDisplay(){
+  this.setState({
+    displayValue:'0'
+  })
+}
+inputDigit(digit){ 
+  const{displayValue, waitingForOperand} = this.state
+
+  if(waitingForOperand){
+    this.setState({
+      displayValue: String(digit),
+      waitingForOperand:false
+    })
+  }
+  else{
+
+    this.setState({
+      displayValue:displayValue === "0"? String(digit) : displayValue + digit
+    });
+
+  }
+}
+inputDot(){
+  const {displayValue, waitingForOperand} = this.state
+
+  if(waitingForOperand){
+    this.setState({
+      displayValue:".",
+      waitingForOperand:false
+    })
+  }
+  else if (displayValue.indexOf('.') === -1){
+    this.setState({
+      displayValue:displayValue + ".",
+      waitingForOperand: false
+    })
+
+  }
+}
+toggleSign(){
+  const { displayValue } = this.state
+  this.setState({
+    displayValue:displayValue.charAt(0)==="-" ? displayValue.substr(1) : "-" + displayValue
+  })
+}
+inputPercent(){
+  const { displayValue } = this.state
+  const value = parseFloat(displayValue)
+
+
+  this.setState({
+    displayValue: String(value / 100)
+  })
+}
+performOperation(nextOperator){
+  const {displayValue, operator, value} = this.state
+  const nextValue = parseFloat (displayValue)
+
+  const operations ={
+    "/" : (prevValue, nextValue) => prevValue / nextValue,
+    "*" : (prevValue, nextValue) => prevValue * nextValue,
+    "+" : (prevValue, nextValue) => prevValue + nextValue,
+    "-" : (prevValue, nextValue) => prevValue - nextValue,
+    "=" : (prevValue, nextValue) => nextValue,
+
+  }
+  if (value === null){
+    this.setState({
+      value:nextValue
+    })
+  }else if(operator){
+    const currentValue = value || 0
+    const computedValue = operations[operator](currentValue, nextValue)
+
+    this.setState({
+      value:computedValue,
+      displayValue:String(computedValue)
+    })
+  }
+  this.setState({
+    waitingForOperand:true,
+    operator:nextOperator
+  })
+}
+
   render() {
+    const { displayValue } = this.state
     return (
       <div className='calculator background'>
-        <div className='display'></div>
+        <div className='display'>
+          <div className='display-text'>{displayValue}</div>
+        </div>
         <div className='center'>
           <div className='row'>
-            <div className='button c container'>
-              <div>C</div>
-            </div>
-            <div className='button c container'>
+            <button className='c container' onClick={() => this.clearDisplay()}>
+              <div>AC</div>
+            </button>
+            <button className='c container' onClick={() =>this.toggleSign()}>
               <div>+/-</div>
-            </div>
-            <div className='button c container'>
+            </button>
+            <button className='c container' onClick={() =>this.inputPercent()}>
               <div>%</div>
-            </div>
-            <div className='button e container'>
+            </button>
+            <button className='e container' onClick={() => this.performOperation('/')}>
               <div>÷</div>
+            </button>
             </div>
-          </div>
           <div className='row'>
-            <div className='button d container'>
+            <button className='d container' onClick={() => this.inputDigit(7)}>
               <div>7</div>
-            </div>
-            <div className='button d container'>
+            </button>
+            <button className='d container' onClick={() => this.inputDigit(8)}>
               <div>8</div>
-            </div>
-            <div className='button d container'>
+            </button>
+            <button className='d container' onClick={() => this.inputDigit(9)}>
               <div>9</div>
-            </div>
-            <div className='button e container'>
+            </button>
+            <button className='e container' onClick={() => this.performOperation('*')}>
               <div>x</div>
+            </button>
             </div>
-          </div>
           <div className='row'>
-            <div className='button d container'>
+            <button className='d container' onClick={() => this.inputDigit(4)}>
               <div>4</div>
-            </div>
-            <div className='button d container'>
+            </button>
+            <button className='d container' onClick={() => this.inputDigit(5)}>
               <div>5</div>
-            </div>
-            <div className='button d container'>
+            </button>
+            <button className='d container' onClick={() => this.inputDigit(6)}>
               <div>6</div>
-            </div>
-            <div className='button e container'>
+            </button>
+            <button className='e container' onClick={() => this.performOperation('-')}>
               <div>-</div>
-            </div>
+            </button>
           </div>
           <div className='row'>
-            <div className='button d container'>
+            <button className='d container' onClick={() => this.inputDigit(1)}>
               <div>1</div>
-            </div>
-            <div className='button d container'>
+            </button>
+            <button className='d container' onClick={() => this.inputDigit(2)}>
               <div>2</div>
-            </div>
-            <div className='button d container'>
+            </button>
+            <button className='d container' onClick={() => this.inputDigit(3)}>
               <div>3</div>
-            </div>
-            <div className='button e container'>
+            </button>
+            <button className='e container' onClick={() => this.performOperation('+')}>
               <div>+</div>
-            </div>
+            </button>
           </div>
           <div className='row'>
-            <div className='button d container'>
+            <zero className='d container' onClick={() => this.inputDigit(0)}>
               <div>0</div>
-            </div>
-            <div className='button d container'>
-              <div>x10</div>
-            </div>
-            <div className='button d container'>
+            </zero>
+            <button className='d container' onClick={()=> this.inputDot()}>
               <div>.</div>
-            </div>
-            <div className='button e container'>
+            </button>
+            <button className='e container' onClick={() => this.performOperation('=')}>
               <div>=</div>
-            </div>
+            </button>
           </div>          
         </div>
       </div>
@@ -84,4 +176,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default Calculator;
